7e78d2baac72acb94e1f8c3ae2e5992bfcc4c44a
Dohyun Kim <dkmqflx@gmail.com>
2022-08-19 16:11:25 +0900

    chore: add markdown libraries

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,6 +21,52 @@ module.exports = {
         path: `${__dirname}/static`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents`,
+      },
+    },
+
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-smartypants',
+            options: {
+              dashes: 'oldschool',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              quality: 100,
+              withWebp: true,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {},
+          },
+          {
+            resolve: 'gatsby-remark-external-links',
+            options: {
+              target: '_blank',
+              rel: 'nofollow',
+            },
+          },
+        ],
+      },
+    },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,60 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
-function index() {
-  return <div>index</div>
+type IndexPageProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: [
+        {
+          node: {
+            id: string
+            frontmatter: {
+              title: string
+              summary: string
+              date: string
+              categories: string[]
+            }
+          }
+        },
+      ]
+    }
+  }
+}
+
+function index({
+  data: {
+    allMarkdownRemark: {
+      edges: [
+        {
+          node: {
+            frontmatter: { title, summary, date, categories },
+          },
+        },
+      ],
+    },
+  },
+}: IndexPageProps) {
+  return <div>{`${title}, ${summary}, ${date}, ${categories}`}</div>
 }
 
 export default index
+
+export const getPostList = graphql`
+  query getPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+          }
+        }
+      }
+    }
+  }
+`
