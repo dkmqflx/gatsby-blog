a32329388bd1a691eab589d2b778a62faac6d0c5
Dohyun Kim <dkmqflx@gmail.com>
2022-08-22 22:41:50 +0900

    chore: set import alias

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,18 @@
 const path = require('path')
 
-exports.onCreateWebpackConfig = ({ actions }) => {
+// Setup Import Alias
+exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
+  const output = getConfig().output || {}
+
   actions.setWebpackConfig({
+    output,
     resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      alias: {
+        components: path.resolve(__dirname, 'src/components'),
+        utils: path.resolve(__dirname, 'src/utils'),
+        hooks: path.resolve(__dirname, 'src/hooks'),
+        types: path.resolve(__dirname, 'src/types'),
+      },
     },
   })
 }
