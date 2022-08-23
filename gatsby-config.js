8c252f91df8c6af21ef76501c3aeaf204bc7df60
Dohyun Kim <dkmqflx@gmail.com>
2022-08-23 14:03:31 +0900

    feat: chore: add robots plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,5 +103,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
   ],
 }
