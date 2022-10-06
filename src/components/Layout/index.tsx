17f1b0a1e10f34d34fec2fe497d62eb1ba1a57c4
Dohyun Kim <dkmqflx@gmail.com>
2022-10-06 17:58:59 +0900

    feat: change layout px unit to em, rem

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,8 +4,9 @@ import styled from '@emotion/styled'
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  width: 768px;
-  padding: 20px 0;
+  width: 48em;
+  max-width: 48em;
+  padding: 1.25em 0;
 `
 
 const Layout = ({ children }: { children: ReactNode }) => {
