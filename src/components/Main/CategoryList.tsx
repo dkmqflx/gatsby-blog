752ba1a738a8022cc5957ee2cb1b0b19a0c5107c
Dohyun Kim <dkmqflx@gmail.com>
2023-02-01 02:54:30 +0900

    feat: show all categories

--- src/components/Main/CategoryList.tsx
+++ src/components/Main/CategoryList.tsx
@@ -1,18 +1,8 @@
-import React, {
-  MutableRefObject,
-  ReactNode,
-  useCallback,
-  useEffect,
-  useRef,
-  useState,
-  useMemo,
-} from 'react'
+import { ReactNode, useMemo } from 'react'
 import { Link } from 'gatsby'
 import { CategoryListProps, categoryListType } from 'types/main.types'
 import { PostListItemType } from 'types/post.types'
 import styled from '@emotion/styled'
-import DownArrow from '/static/downArrow.svg'
-import UpArrow from '/static/upArrow.svg'
 
 type CategoryItemProps = {
   active: boolean
@@ -25,11 +15,6 @@ type GatsbyLinkProps = {
 } & CategoryItemProps
 
 const CategoryList = ({ selectedCategory, posts }: CategoryListProps) => {
-  const [more, setMore] = useState(false)
-  const [visible, setVisible] = useState(false)
-  const categoryRef: MutableRefObject<HTMLDivElement | null> =
-    useRef<HTMLDivElement | null>(null)
-
   const categoryList = useMemo(
     () =>
       posts.reduce(
@@ -55,41 +40,8 @@ const CategoryList = ({ selectedCategory, posts }: CategoryListProps) => {
     [],
   )
 
-  const toggle = useCallback(() => {
-    setMore(more => !more)
-
-    if (typeof window !== 'undefined' && categoryRef.current) {
-      const fontSize = Number(
-        window
-          .getComputedStyle(document.body)
-          .getPropertyValue('font-size')
-          .match(/\d+/)![0],
-      )
-
-      categoryRef.current!.style.height =
-        categoryRef.current?.style.height === '2.5rem'
-          ? `${categoryRef.current.scrollHeight * (1 / fontSize)}rem`
-          : '2.5rem'
-    }
-  }, [])
-
-  useEffect(() => {
-    if (
-      categoryRef.current &&
-      categoryRef.current.clientHeight < categoryRef.current.scrollHeight
-    ) {
-      categoryRef.current.style.height = '2.5rem'
-      categoryRef.current.style.transition = 'all 0.3s linear'
-
-      const categorLinks = categoryRef.current.querySelectorAll('a')
-      categorLinks.forEach(link => (link.style.marginBottom = '0.625em'))
-
-      setVisible(true)
-    }
-  }, [])
-
   return (
-    <CategoryListWrapper ref={categoryRef}>
+    <CategoryListWrapper>
       {Object.entries(categoryList).map(([name, count]) => (
         <CategoryLink
           key={name}
@@ -99,13 +51,6 @@ const CategoryList = ({ selectedCategory, posts }: CategoryListProps) => {
           {name}({count})
         </CategoryLink>
       ))}
-
-      {visible &&
-        (more ? (
-          <UpArrowIcon onClick={toggle}>-</UpArrowIcon>
-        ) : (
-          <DownArrowIcon onClick={toggle}>+</DownArrowIcon>
-        ))}
     </CategoryListWrapper>
   )
 }
@@ -115,11 +60,7 @@ export default CategoryList
 const CategoryListWrapper = styled.div`
   display: flex;
   position: relative;
-  margin-bottom: 4em;
-
   flex-wrap: wrap;
-  overflow: hidden;
-  height: 2.5rem;
 
   @media (max-width: 48rem) {
     margin-bottom: 2em;
@@ -131,6 +72,7 @@ const CategoryLink = styled(({ active, ...props }: GatsbyLinkProps) => (
 ))<CategoryItemProps>`
   background-color: var(--button-color);
   margin-right: 1em;
+  margin-bottom: 1em;
   padding: 0.5rem;
   border-radius: 0.25rem;
 
@@ -138,26 +80,5 @@ const CategoryLink = styled(({ active, ...props }: GatsbyLinkProps) => (
 
   &:last-of-type {
     margin-right: 0;
-  }
-`
-
-const UpArrowIcon = styled(UpArrow)`
-  position: absolute;
-  right: 1em;
-  cursor: pointer;
-
-  path {
-    stroke: var(--secondary-color);
-    fill: var(--secondary-color);
-  }
-`
-
-const DownArrowIcon = styled(DownArrow)`
-  position: absolute;
-  right: 1em;
-  cursor: pointer;
-  path {
-    stroke: var(--secondary-color);
-    fill: var(--secondary-color);
   }
 `
