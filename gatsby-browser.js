3e139e5becdea231b2b6ae692b6f0e03f751a087
dkmqflx <dkmqflx@gmail.com>
2023-11-27 23:07:40 +0900

    fix: prismjs theme 추가

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@
  */
 
 // You can delete this file if you're not using it
-// import 'prismjs/themes/prism-tomorrow.css'
+import 'prismjs/themes/prism-tomorrow.css'
 // import 'fontsource-noto-sans-kr'
 // import 'fontsource-noto-sans-kr/500.css'
 // import 'fontsource-noto-sans-kr/700.css'
